186bc43aa31ac51ac9e799d4d462871842ba7c63
Ayfri <pierre.ayfri@gmail.com>
2026-02-04 14:07:57 +0100

    feat(model): Implement MLflow integration for housing price prediction model training.

--- train_model.py
+++ train_model.py
@@ -1,12 +1,26 @@
+
 def build_model():
     import pandas as pd
     from sklearn.linear_model import LinearRegression
-    import joblib
+    from sklearn.model_selection import train_test_split
+    import mlflow
+
+    mlflow.set_experiment("housing_prices")
+    mlflow.sklearn.autolog()
+
     df = pd.read_csv('data/houses.csv')
+
     X = df[['size', 'nb_rooms', 'garden']]
     y = df['price']
-    model = LinearRegression()
-    model.fit(X, y)
-    joblib.dump(model, "output/regression.joblib")
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+    with mlflow.start_run():
+        model = LinearRegression()
+        model.fit(X_train, y_train)
+
+        model.score(X_test, y_test)
+
+        print("Run ID:", mlflow.active_run().info.run_id)
 
 build_model()
